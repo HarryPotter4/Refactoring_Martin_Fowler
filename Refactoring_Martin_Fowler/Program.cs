6e8ec1ddb1e35105c407800d1870eeaa120f100a
Marius Becherer <marius.becherer@gmx.net>
2016-04-26 11:12:42 +0200

    Extract field

--- Refactoring_Martin_Fowler/Program.cs
+++ Refactoring_Martin_Fowler/Program.cs
@@ -14,14 +14,17 @@ namespace Refactoring_Martin_Fowler
             Console.WriteLine("Welcome to the Movie Store");
 
             List<Movie> movie = new List<Movie>();
+            List<Rental> rental = new List<Rental>();
 
             movie.Add(new Movie("movie1", 1));
             movie.Add(new Movie("movie2", 2));
 
-            Rental r1 = new Rental(movie_1, 10);
-            Rental r2 = new Rental(m2, 5);
+            rental.Add(new Rental(movie.ElementAt(1), 10));
+            rental.Add(new Rental(movie.ElementAt(2), 5));
+           
             Customer c1 = new Customer("joe");
             c1.addRental(r1); c1.addRental(r2);
+
             Console.WriteLine("Let's get the Statement");
             result = c1.statement();
             Console.Write(result);
