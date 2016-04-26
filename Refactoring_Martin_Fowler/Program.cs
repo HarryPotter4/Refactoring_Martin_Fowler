4675e8e3783a967d0e9658c52275ddde225c0b16
Marius Becherer <marius.becherer@gmx.net>
2016-04-26 11:06:11 +0200

    Rename m1 to movie_1

--- Refactoring_Martin_Fowler/Program.cs
+++ Refactoring_Martin_Fowler/Program.cs
@@ -12,9 +12,9 @@ namespace Refactoring_Martin_Fowler
         {
             String result;
             Console.WriteLine("Welcome to the Movie Store");
-            Movie m1 = new Movie("movie1", 1);
+            Movie movie_1 = new Movie("movie1", 1);
             Movie m2 = new Movie("movie2", 2);
-            Rental r1 = new Rental(m1, 10);
+            Rental r1 = new Rental(movie_1, 10);
             Rental r2 = new Rental(m2, 5);
             Customer c1 = new Customer("joe");
             c1.addRental(r1); c1.addRental(r2);
